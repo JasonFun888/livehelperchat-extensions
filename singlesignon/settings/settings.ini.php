dcd31eb0cbe14c7044796d6199e770794d8360fa
Jason <jasonfun888@gmail.com>
2019-12-18 14:53:29 +0800

    Stage-环境兼容：配置文件分支

--- singlesignon/settings/settings.ini.php
+++ singlesignon/settings/settings.ini.php
@@ -2,7 +2,7 @@
 
 return array( 
 		'sso_location' => '../../SimpleSAMLphp/lib/_autoload.php',
-		'post_host' => 'https://chat.segwaydiscovery.com/chatclosehandler.php',
+		'post_host' => 'https://stage-ap-chat.segwaydiscovery.com/chatclosehandler.php',
         'attr_map' => array(   
             'email'     => 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn',
             'username'  => 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn',
